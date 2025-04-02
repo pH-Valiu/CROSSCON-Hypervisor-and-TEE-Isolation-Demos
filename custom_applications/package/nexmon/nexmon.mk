dba294912d9bd0e08bf0e0f9ffa1a7a0bc66817b
pH-Valiu <sdswae374@gmail.com>
2025-04-02 13:16:48 +0200

    First complete draft of building nexmon in the buildroot pipeline

--- custom_applications/package/nexmon/nexmon.mk
+++ custom_applications/package/nexmon/nexmon.mk
@@ -4,21 +4,31 @@
 #
 ####################################################################
 
-NEXMON_VERSION = 5.10.92-v7+
-NEXMON_SOURCE = $(NEXMON_VERSION).tar.xz
-NEXMON_SITE = https://github.com/nexmonster/nexmon_csi_bin/tree/main/base
-NEXMON_LICENSE = MIT-1
+NEXMON_VERSION = 1.0
+NEXMON_SITE = $(call github,pH-Valiu,nexmon,v$(NEXMON_VERSION))
+NEXMON_DEPENDENCIES = lib
+NEXMON_LICENSE = GPL-3.0
 NEXMON_INSTALL_STAGING = NO
+NEXMON_DEPENDENCIES = qpdf gmp
 
-KERNEL_VERSION = 5.10.92
+define NEXMON_BUILD_CMDS
+	export KERNEL_VERSION = 5.10.92; \
+	export ARCH = arm; \
+	export SUBARCH = arm; \
+	export KERNEL = kernel7; \
+	export HOSTUNAME = Linux; \
+	export PLATFORMUNAME = aarch6; \
+	export NEXMON_ROOT = $(@D); \
+	export CC = $(NEXMON_ROOT)/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-armv7l/bin/arm-none-eabi-; \
+	export CCPLUGIN = $(NEXMON_ROOT)/buildtools/gcc-nexmon-plugin/nexmon.so; \
+	export ZLIBFLATE = "zlib-flate -compress"; \
+	export NEXMON_SETUP_ENV = 1; \
+	export Q=@; \
+	$(MAKE) install-firmware -C $(@D)/patches/bcm43455c0/7_45_189/nexmon_csi; \
+	$(MAKE) -C $(@D)/utilities/nexutil; \
+	$(MAKE) install -C $(@D)/utilities/nexutil; \
+endef
 
-#define NEXMON_BUILD_CMDS
-#	$(MAKE) -C $(@D) all
-#endef
-
-#define NEXMON_CSI_BIN_EXTRACT_CMDS
-#    tar -xvJf $(@D)/$(KERNEL_VERSION).tar.xz -C $(@D)
-#endef
 
 define NEXMON_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_VERSION)/nexutil/nexutil $(TARGET_DIR)/usr/local/bin/nexutil
