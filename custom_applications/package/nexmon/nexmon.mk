57e3dc7b876b089d67d8d0771628d551dd2f00b8
pH-Valiu <sdswae374@gmail.com>
2025-05-12 13:50:37 +0200

    Modified nexmon.mk to properly download precompiled binaries for kernel 5.10.92-v8+

--- custom_applications/package/nexmon/nexmon.mk
+++ custom_applications/package/nexmon/nexmon.mk
@@ -5,37 +5,25 @@
 ####################################################################
 
 NEXMON_VERSION = 1.0
-NEXMON_SITE = $(call github,pH-Valiu,nexmon,v$(NEXMON_VERSION))
-NEXMON_DEPENDENCIES = lib
-NEXMON_LICENSE = GPL-3.0
+NEXMON_KERNEL_VERSION = 5.10.92-v8+
+NEXMON_SOURCE = $(KERNEL_VERISON).tar.xz
+NEXMON_SITE = https://raw.githubusercontent.com/pH-Valiu/nexmon_csi_bin/main/base
+NEXMON_LICENSE = MIT-1
 NEXMON_INSTALL_STAGING = NO
 NEXMON_DEPENDENCIES = qpdf gmp
 
-define NEXMON_BUILD_CMDS
-	export KERNEL_VERSION = 5.10.92; \
-	export ARCH = arm; \
-	export SUBARCH = arm; \
-	export KERNEL = kernel7; \
-	export HOSTUNAME = Linux; \
-	export PLATFORMUNAME = aarch6; \
-	export NEXMON_ROOT = $(@D); \
-	export CC = $(NEXMON_ROOT)/buildtools/gcc-arm-none-eabi-5_4-2016q2-linux-armv7l/bin/arm-none-eabi-; \
-	export CCPLUGIN = $(NEXMON_ROOT)/buildtools/gcc-nexmon-plugin/nexmon.so; \
-	export ZLIBFLATE = "zlib-flate -compress"; \
-	export NEXMON_SETUP_ENV = 1; \
-	export Q=@; \
-	$(MAKE) install-firmware -C $(@D)/patches/bcm43455c0/7_45_189/nexmon_csi; \
-	$(MAKE) -C $(@D)/utilities/nexutil; \
-	$(MAKE) install -C $(@D)/utilities/nexutil;
+KERNEL_VERSION = 5.10.92-v8+
+
+define NEXMON_EXTRACT_CMDS
+	tar -xvJf $(@D)/$(NEXMON_KERNEL_VERSION).tar.xz -C $(@D)
 endef
 
-
 define NEXMON_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_VERSION)/nexutil/nexutil $(TARGET_DIR)/usr/local/bin/nexutil
-	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_VERSION)/makecsiparams/makecsiparams $(TARGET_DIR)/usr/local/bin/mcp
-	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_VERSION)/makecsiparams/makecsiparams $(TARGET_DIR)/usr/local/bin/makecsiparams
-	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_VERSION)/patched/brcmfmac43455-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.bin
-	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_VERSION)/patched/brcmfmac.ko $(TARGET_DIR)/lib/modules/$(KERNEL_VERSION)/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko
+	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_KERNEL_VERSION)/nexutil/nexutil $(TARGET_DIR)/usr/local/bin/nexutil
+	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_KERNEL_VERSION)/makecsiparams/makecsiparams $(TARGET_DIR)/usr/local/bin/mcp
+	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_KERNEL_VERSION)/makecsiparams/makecsiparams $(TARGET_DIR)/usr/local/bin/makecsiparams
+	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_KERNEL_VERSION)/patched/brcmfmac43455-sdio.bin $(TARGET_DIR)/lib/firmware/brcm/brcmfmac43455-sdio.bin
+	$(INSTALL) -D -m 0755 $(@D)/$(NEXMON_KERNEL_VERSION)/patched/brcmfmac.ko $(TARGET_DIR)/lib/modules/$(KERNEL_VERSION)/kernel/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko
 endef
 
 
